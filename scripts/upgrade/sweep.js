99edc767b4eb5e3b5169ae2cd877095ed6e20e09
maxcoto <maxiperezc@gmail.com>
2023-09-04 22:46:45 -0300

    updates script to upgrade SWEEP

--- scripts/upgrade/sweep.js
+++ scripts/upgrade/sweep.js
@@ -1,24 +1,17 @@
-const { ethers, upgrades } = require('hardhat');
-const { network, addresses } = require('../../utils/address');
+const { ethers } = require('hardhat');
+const { network } = require('../../utils/address');
 
 async function main() {
-	let deployer = '';
-
-	if (network.type === "0") { // local
-		[deployer] = await ethers.getSigners();
-		deployer = deployer.address;
-	} else {
-		deployer = addresses.owner;
-	}
+	[deployer] = await ethers.getSigners();
+	deployer = deployer.address;
 
 	console.log(`Updating contracts on ${network.name} with the account: ${deployer}`);
 
-	let proxyAddress = addresses.sweep;
-	const sweepInstance = await ethers.getContractFactory("SweepCoin");
-	// await upgrades.forceImport(proxyAddress, sweepInstance);
-	await upgrades.upgradeProxy(proxyAddress, sweepInstance);
+	const Sweep = await ethers.getContractFactory("SweepCoin");
+	const new_imp = await Sweep.deploy();
 	
-	console.log("Sweep Upgraded!");
+	console.log("new sweep implementation", new_imp.address);
+	console.log(`\nnpx hardhat verify --network ${network.name} ${new_imp.address}`);
 }
 
 main();
