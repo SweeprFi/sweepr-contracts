e24b484ba82492c99edaa288384950a3bdd54206
maxcoto <maxiperezc@gmail.com>
2023-09-29 10:38:15 -0300

    fixes tests

--- test/truffle-fixture.js
+++ test/truffle-fixture.js
@@ -11,21 +11,21 @@ const USDC = artifacts.require("ERC20");
 module.exports = async () => {
   // Get the necessary instances
   // ======================================================
-  // usdc_instance = await USDC.at(addresses.usdc);
-  // sweep_instance = await SWEEP.at(addresses.sweep);
-  // sweepr_instance = await SWEEPR.at(addresses.sweepr);
-  // governance_instance = await Governor.at(addresses.governance);
-  // // uniswap_oracle_instance = await UniswapOracle.at(addresses.uniswapOracle);
-  // // ----------------------------------------------
-  // USDC.setAsDeployed(usdc_instance);
-  // SWEEP.setAsDeployed(sweep_instance);
-  // SWEEPR.setAsDeployed(sweepr_instance);
-  // Governor.setAsDeployed(governance_instance);
-  // // UniswapOracle.setAsDeployed(uniswap_oracle_instance);
-  // // ----------------------------------------------
-  // sweep_owner = await sweep_instance.owner();
-  // await sendEth(sweep_owner);
-  // await sendEth(addresses.usdc);
-  // await sendEth(addresses.borrower);
-  // await sendEth(addresses.multisig);
+  usdc_instance = await USDC.at(addresses.usdc);
+  sweep_instance = await SWEEP.at(addresses.sweep);
+  sweepr_instance = await SWEEPR.at(addresses.sweepr);
+  governance_instance = await Governor.at(addresses.governance);
+  // uniswap_oracle_instance = await UniswapOracle.at(addresses.uniswapOracle);
+  // ----------------------------------------------
+  USDC.setAsDeployed(usdc_instance);
+  SWEEP.setAsDeployed(sweep_instance);
+  SWEEPR.setAsDeployed(sweepr_instance);
+  Governor.setAsDeployed(governance_instance);
+  // UniswapOracle.setAsDeployed(uniswap_oracle_instance);
+  // ----------------------------------------------
+  sweep_owner = await sweep_instance.owner();
+  await sendEth(sweep_owner);
+  await sendEth(addresses.usdc);
+  await sendEth(addresses.borrower);
+  await sendEth(addresses.multisig);
 }
